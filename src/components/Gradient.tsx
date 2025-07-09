c592653a532f67eb3af2463157200c2224325092
michael <michael@iseard.media>
2025-07-09 16:07:06 +0200

    Update gradients for compatibility with tailwind@4.1.x

--- src/components/Gradient.tsx
+++ src/components/Gradient.tsx
@@ -2,14 +2,14 @@ import { clsx } from 'clsx'
 import React from 'react'
 import { GridPattern } from '@/components/GridPattern'
 
-let variant1 =
-  'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#2BFF88] from-10% via-[#2BD2FF] via-52% to-[#FA8BFF] to-90% sm:bg-[linear-gradient(190deg,var(--tw-gradient-stops))]'
-let variant2 =
-  'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#FA8BFF] from-0% via-[#FF8E25] via-50% to-[#EFFFC8] to-100% sm:bg-[linear-gradient(45deg,var(--tw-gradient-stops))]'
-let variant3 =
-  'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#fff1be] from-28% via-[#ee87cb] via-70% to-[#b060ff]'
-
-let active = variant2
+let variants = {
+  pinkOrangeGreen:
+    'bg-linear-115 from-[#2BFF88] from-10% via-[#2BD2FF] via-52% to-[#FA8BFF] sm:bg-linear-190',
+  tealPurpleBlue:
+    'bg-linear-115 from-[#FA8BFF] from-0% via-[#FF8E25] via-50% to-[#EFFFC8] sm:bg-linear-45',
+  template:
+    'bg-linear-115 from-[#fff1be] from-28% via-[#ee87cb] via-70% to-[#b060ff] sm:bg-linear-145',
+}
 
 export function Gradient({
   className,
@@ -23,7 +23,10 @@ export function Gradient({
     '[mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)]'
   return (
     <div className={clsx(className, '-z-10 overflow-hidden')}>
-      <div {...props} className={clsx(active, 'h-full w-full')} />
+      <div
+        {...props}
+        className={clsx(variants.tealPurpleBlue, 'h-full w-full')}
+      />
       {showGrid && (
         <GridPattern
           className="absolute inset-2 top-0 left-0 h-full w-full fill-neutral-50 stroke-neutral-950/5 opacity-10"
@@ -42,7 +45,7 @@ export function GradientBackground() {
       <div
         className={clsx(
           'absolute -top-44 -right-60 h-60 w-[36rem] transform-gpu',
-          active,
+          variants.tealPurpleBlue,
           'rotate-[-10deg] rounded-full blur-3xl',
         )}
       />
